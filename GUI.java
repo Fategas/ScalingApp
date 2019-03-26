224adbe96b636d14b27a39ac8ed87f445db6b86b
fategas <normalplek@gmail.com>
2019-03-26 22:48:10 +0000

    Added TODOs to both files, updated and/or added a proper javadoc.

--- GUI.java
+++ GUI.java
@@ -2,28 +2,47 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 /**
- * This class will show a gui and use the existing scaleConverter class
+ * This class will show a Graphical interface and use the existing ScaleConverter class
  * to convert scales.
+ * Users will be able to chose the initial number, the original scale
+ * and the scale they want to have this number converted into.
+ * It should display a textarea that will show the converted number after clicking submit.
+ *
+ * TODO: javadoc, bug fixes, convert actionlistener into a infinite loop,
+ * change layout around to make it more plesant to read
  *
  * @author Dawid Michniuk
  * @version 0.01a
  */
 public class GUI extends JFrame
 {
+    //initialize variables
     private ScaleConverter converter;
     private JButton bChange;
     private JTextField textforStartingNumber,textforStartingScale,textforWantedScale,textConverted;
     /**
-     * Constructor for objects of class GUI
+     * Calls JFrame's constructor which allows us to set the window's name.
+     * it then makes the frame and shows it to the user using an appropriate method.
      */
     public GUI()
     {
+        //window label
         super("SCALE!");
         makeFrame();
         showFrame();
 
     }
 
+    /**
+    * When the button is pressed, it should getText (numbers in this case) inside every textarea,
+    * use ScaleConverter's appropriate method to set and then convert to set scale
+    * and set this as a text in textConverted textarea (which I somehow will disable as area user can type in)
+    * TODO: change actionlistener to a while(true) loop.
+    *
+    * Textareas are set to width of 5 and then added to the container, so is the bChange (buttonChange).
+    *
+    *
+    */
     public void makeFrame()
     {
         setLayout(new FlowLayout());
@@ -60,6 +79,11 @@ public class GUI extends JFrame
         add(bChange);
     }
 
+    /**
+    * Simply sets the size of the window, what it should do when users clicks close
+    * (By default it would run in the background, this forces it to actually close the program)
+    * Location will be the center of the screen, and only then it will become visible to the user.
+    */
      public void showFrame()
      {
          setSize(400,400);
--- ScaleConverter.java
+++ ScaleConverter.java
@@ -1,26 +1,30 @@
-import java.util.Scanner;
-
 /**
  * This class will calculate how long something
- * done in 1 scale will be in another scale.
+ * done in 1 scale will be in an another scale.
+ *
+ * TODO: lambda function for converter to make it faster on multicore PCs, port it to android.
  *
  * @author Dawid Michniuk
- * @version 0.01a
+ * @version 0.04a
  */
 public class ScaleConverter
 {
-    private Scanner keyboard;
+    //initialize variables
     private double startingNumber,convertedNumber;
     private int scaleFrom,scaleTo;
+
+    /**
+    * Empty constructor since we don't need to do anything.
+    */
     public ScaleConverter()
     {
-        startingNumber=0;
-        convertedNumber=0;
-        scaleFrom=0;
-        scaleTo=0;
-        keyboard = new Scanner(System.in);
+        //Nothing to set here.
     }
 
+    /**
+    * Tries to set the new value, will catch an error (probably if param is not int)
+    * @param scaleFrom - the starting scale we want to convert FROM.
+    */
     public void setScaleFrom(int scaleFrom)
     {
         try
@@ -32,6 +36,11 @@ public class ScaleConverter
             System.out.println("Error!" + e);
         }
     }
+
+    /**
+    * Tries to set the new value, will catch an error (probably if param is not int)
+    * @param scaleTo - scale we want to convert the number INTO.
+    */
     public void setScaleTo(int scaleTo)
     {
         try
@@ -43,6 +52,10 @@ public class ScaleConverter
             System.out.println("Error!" + e);
         }
     }
+    /**
+    * Tries to set the new value, will catch an error (probably if param is not int)
+    * @param startingNumber - the starting number we want to convert to a different scale.
+    */
     public void setStartingNumber(int startingNumber)
     {
         try
@@ -57,7 +70,15 @@ public class ScaleConverter
 
     public double convert()
     {
+        try
+        {
             convertedNumber = (startingNumber * scaleFrom) / scaleTo;
             return convertedNumber;
+        }
+        catch(Exception e)
+        {
+            System.out.println("This will probably print if you set scaleTo to zero!");
+            System.out.println("Just for debugging: " + e);
+        }
     }
 }
